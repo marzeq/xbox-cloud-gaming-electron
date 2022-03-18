09e465c6c6ec57ba7145bcb6be2a8a6caae3f98a
marzeq <58303665+marzeq@users.noreply.github.com>
2022-03-18 14:16:47 +0100

    Move to tabs with size 4 and remove semicolons

--- scripts/main.js
+++ scripts/main.js
@@ -1,83 +1,83 @@
-const { app, globalShortcut, BrowserWindow } = require("electron");
-const path = require("path");
-const userAgent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36";
+const { app, globalShortcut, BrowserWindow } = require("electron")
+const path = require("path")
+const userAgent = "Mozilla/5.0 (X11 Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36"
 
-var isFullScreen = false;
+var isFullScreen = false
 
-app.commandLine.appendSwitch("enable-features", "VaapiVideoDecoder");
+app.commandLine.appendSwitch("enable-features", "VaapiVideoDecoder")
 
 function createWindow() {
-  const mainWindow = new BrowserWindow({
-    webPreferences: {
-      preload: path.join(__dirname, "preload.js"),
-      contextIsolation: false,
-      nativeWindowOpen: false
-    },
-    title: "Xbox Cloud Gaming"
-  });
+	const mainWindow = new BrowserWindow({
+		webPreferences: {
+			preload: path.join(__dirname, "preload.js"),
+			contextIsolation: false,
+			nativeWindowOpen: false
+		},
+		title: "Xbox Cloud Gaming"
+	})
 
-  mainWindow.loadURL("https://www.xbox.com/en-US/play");
+	mainWindow.loadURL("https://www.xbox.com/en-US/play")
 
-  /*
-  mainWindow.webContents.on("will-navigate", (event, url) => {
-    console.log("will-navigate", url);
-    event.preventDefault();
-  });
-  */
+	/*
+	mainWindow.webContents.on("will-navigate", (event, url) => {
+		console.log("will-navigate", url)
+		event.preventDefault()
+	})
+	*/
 }
 
 app.whenReady().then(() => {
-  createWindow();
+	createWindow()
 
-  app.on("activate", function () {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow();
-    }
-  });
+	app.on("activate", function () {
+		if (BrowserWindow.getAllWindows().length === 0) {
+			createWindow()
+		}
+	})
 
-  globalShortcut.register("F11", () => {
-    isFullScreen = BrowserWindow.getAllWindows()[0].isFullScreen();
-    if (isFullScreen) {
-      BrowserWindow.getAllWindows()[0].setFullScreen(false);
-      isFullScreen = false;
-    } else {
-      BrowserWindow.getAllWindows()[0].setFullScreen(true);
-      isFullScreen = true;
-    }
-  });
-});
+	globalShortcut.register("F11", () => {
+		isFullScreen = BrowserWindow.getAllWindows()[0].isFullScreen()
+		if (isFullScreen) {
+			BrowserWindow.getAllWindows()[0].setFullScreen(false)
+			isFullScreen = false
+		} else {
+			BrowserWindow.getAllWindows()[0].setFullScreen(true)
+			isFullScreen = true
+		}
+	})
+})
 
 app.on("browser-window-created", function (e, window) {
-  window.setBackgroundColor("#1A1D1F");
-  window.setMenu(null);
-  window.webContents.setUserAgent(userAgent);
+	window.setBackgroundColor("#1A1D1F")
+	window.setMenu(null)
+	window.webContents.setUserAgent(userAgent)
 
-  window.on("leave-full-screen", function (e, win) {
-    if (isFullScreen) {
-      BrowserWindow.getAllWindows()[0].setFullScreen(true);
-    }
-  });
+	window.on("leave-full-screen", function (e, win) {
+		if (isFullScreen) {
+			BrowserWindow.getAllWindows()[0].setFullScreen(true)
+		}
+	})
 
-  window.on("page-title-updated", function (e, title) {
-    // cancel event
-    e.preventDefault();
-    console.log("page-title-updated", title);
-    if (title.includes("|   Xbox Cloud Gaming")) {
-      window.setFullScreen(true);
-      isFullScreen = true;
-    } else {
-      window.setFullScreen(false);
-      isFullScreen = false;
-    }
-  });
-});
+	window.on("page-title-updated", function (e, title) {
+		// cancel event
+		e.preventDefault()
+		console.log("page-title-updated", title)
+		if (title.includes("|   Xbox Cloud Gaming")) {
+			window.setFullScreen(true)
+			isFullScreen = true
+		} else {
+			window.setFullScreen(false)
+			isFullScreen = false
+		}
+	})
+})
 
 app.on("will-quit", () => {
-  globalShortcut.unregisterAll();
-});
+	globalShortcut.unregisterAll()
+})
 
 app.on("window-all-closed", function () {
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
-});
+	if (process.platform !== "darwin") {
+		app.quit()
+	}
+})
