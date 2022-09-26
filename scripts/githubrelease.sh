c3e4b6f495cf4a8452fb1d24e70f293c0743e703
marzeq <marzeqmarzeq@gmail.com>
2022-09-26 23:56:24 +0200

    v1.0.11

--- scripts/githubrelease.sh
+++ scripts/githubrelease.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 VERSION=$(awk -F'"' '/"version": ".+"/{ print $4; exit; }' package.json)
+git commit -am "v$VERSION"
+git push
 gh release create $VERSION dist/xbox-cloud-gaming-electron_$VERSION.* --generate-notes
--- scripts/release.sh
+++ scripts/release.sh
@@ -1,5 +1,1 @@
-tsc
-electron-builder --publish never
-
-sh scripts/githubrelease.sh
-sh scripts/aurrelease.sh
+tsc && electron-builder --publish never && sh scripts/githubrelease.sh && sh scripts/aurrelease.sh
