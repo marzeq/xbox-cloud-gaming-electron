538b3c905fd9278dce8defc401a9240f6bb0a8a9
marzeq <marzeqmarzeq@gmail.com>
2022-09-28 21:59:11 +0200

    refactor: remove template-tags dep

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { app, globalShortcut, BrowserWindow, shell } from "electron"
 import path from "path"
-import { javascript, css } from "template-tags"
 import { rpcLogin } from "./rpc"
 
 const userAgentWindows = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5026.0 Safari/537.36 Edg/103.0.1254.0",
@@ -101,16 +100,16 @@ app.on("browser-window-created", async (_, window) => {
 
     const injectCode = () => {
         if (!process.argv.includes("--gpu-info"))
-            window.webContents.insertCSS(css`
+            window.webContents.insertCSS(/*css*/`
                 ::-webkit-scrollbar { display: none; }
             `)
         
         if (!process.argv.includes("--dont-hide-pointer"))
-            window.webContents.insertCSS(css`
+            window.webContents.insertCSS(/*css*/`
                 .no-pointer { cursor: none; }
             `)
 
-            window.webContents.executeJavaScript(javascript`
+            window.webContents.executeJavaScript(/*javascript*/`
                 document.addEventListener("mousemove", () => {
                     document.querySelectorAll("*").forEach((element) => {
                         element.classList.remove("no-pointer")
